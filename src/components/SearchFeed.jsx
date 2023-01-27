4589da7778cc6524ec94ccb804fa1d8245af1bd9
Jonathan <jdfp.master@gmail.com>
2023-01-26 22:34:28 -0500

    SeachBar component begining

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState, useEffect} from 'react';
+import { Box, Typography } from '@mui/material';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import {Videos} from './';
+
 
 const SearchFeed = () => {
+
+    const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${}`)
+    .then((data) => setVideos(data.items))
+  }, []);
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY:'auto', height: '90vh',flex:2}}>
+      <Typography 
+        variant='h4'
+        fontWeight='bold' mb={2} sx={{color:'white'}} >
+        {selectedCategory} <span style={{color:'#F31503'}}>Videos</span>
+      </Typography>
+
+      <Videos videos={videos}/>
+    </Box>
+    
   )
 }
 
