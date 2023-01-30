6d03b17979eed664e08a50dd9ca60e456066fa27
Jonathan <jdfp.master@gmail.com>
2023-01-29 21:10:14 -0500

    VideoDetail Component almost finished

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {Stack, Box} from '@mui/material';
 
 import {VideoCard, ChannelCard} from './';
@@ -5,13 +6,13 @@ import {VideoCard, ChannelCard} from './';
 const Videos = ({videos}) => {
 
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-      {videos.map((item , idx) =>(
+    <Stack direction={"row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
         <Box key={idx}>
-          { item.id.videoId && <VideoCard video = {item}/> }
-          { item.id.channelId && <ChannelCard channelDetail = {item}/> }
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-      ) )}
+      ))}
     </Stack>
   )
 }
